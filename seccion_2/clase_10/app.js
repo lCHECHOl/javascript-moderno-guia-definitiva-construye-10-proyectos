51801a2ea7e1f14bf8849c4d617a9727174880d6
checho <checho.ch55ll@hotmail.com>
2019-08-06 19:43:11 -0500

    Ejemplos de variables con let y const

---
+++ seccion_2/clase_10/app.js
@@ -0,0 +1,12 @@
+let carrito = 'libro 1';
+
+carrito = 'Libro 2';
+
+console.log(carrito);
+
+
+const sabor = 'kola'; // solo se usa cuando sabes que el valor no va a cambiar
+
+console.log(sabor);
+
+
