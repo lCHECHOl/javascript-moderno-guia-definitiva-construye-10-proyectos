7f88bd884ac9d17b1c138255402cc7e48d13401e
checho <checho.ch55ll@hotmail.com>
2019-08-06 19:35:04 -0500

    se define una forma de crear variables en java script

---
+++ seccion_2/clase_9/app.js
@@ -0,0 +1,22 @@
+// var let const
+
+var nombre = 'Checho';
+
+console.log(nombre);
+
+var apellido;
+apellido = 'felete';
+
+console.log(apellido);
+
+var carrito = 'Libro 1';
+    carrito_uno = 'Libro 2';
+
+console.log(carrito);
+console.log(carrito_uno);
+
+var primerNombre = 'albeiro'; // camelcase
+var primer_nombre = 'albeiro'; // underscore
+var PrimerNombre = 'albeiro'; // pascal case
+
+    
